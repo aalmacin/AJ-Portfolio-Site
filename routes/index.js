078e2ecec67076a96774f5f5ddf293025e2abc75
AJ Almacin <aldrinjerome19@gmail.com>
2015-10-05 01:02:46 -0400

    Added about me content

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' , subtitle: 'Welcome to my site.', active: 'home'});
 });
 router.get('/aboutMe', function(req, res, next) {
-  res.render('aboutMe', { title: 'About Me' });
+  res.render('aboutMe', { title: 'About Me' , subtitle: 'Everything you need to know', active: 'aboutMe' });
 });
 router.get('/projects', function(req, res, next) {
-  res.render('projects', { title: 'Projects' });
+  res.render('projects', { title: 'Projects' , subtitle: 'Some of my best works', active: 'projects' });
 });
 router.get('/services', function(req, res, next) {
-  res.render('services', { title: 'Services' });
+  res.render('services', { title: 'Services' , subtitle: '', active: 'services' });
 });
 router.get('/contactMe', function(req, res, next) {
-  res.render('contactMe', { title: 'Contact Me' });
+  res.render('contactMe', { title: 'Contact Me' , subtitle: '', active: 'contactMe' });
 });
 
 module.exports = router;
